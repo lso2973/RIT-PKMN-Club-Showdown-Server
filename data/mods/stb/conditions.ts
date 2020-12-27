bde9282df7e1c4fc91b3808abc8c785e5b5444aa
Br(i)an Flakes <brh3243@rit.edu>
2020-12-27 16:49:02 -0500

    added stub for stb mod

---
+++ data/mods/stb/conditions.ts
@@ -0,0 +1,47 @@
+import {FS} from '../../../lib/fs';
+import {toID} from '../../../sim/dex-data';
+
+// Used in many abilities, placed here to reduce the number of updates needed and to reduce the chance of errors
+const STRONG_WEATHERS = ['desolateland', 'primordialsea', 'deltastream', 'heavyhailstorm', 'winterhail'];
+
+// Similar to User.usergroups. Cannot import here due to users.ts requiring Chat
+// This also acts as a cache, meaning ranks will only update when a hotpatch/restart occurs
+const usergroups: {[userid: string]: string} = {};
+const usergroupData = FS('config/usergroups.csv').readIfExistsSync().split('\n');
+for (const row of usergroupData) {
+	if (!toID(row)) continue;
+
+	const cells = row.split(',');
+	if (cells.length !== 2) throw new Error(`Invalid entry when parsing usergroups.csv`);
+	usergroups[toID(cells[0])] = cells[1].trim() || ' ';
+}
+
+export function getName(name: string): string {
+	const userid = toID(name);
+	if (!userid) throw new Error('No/Invalid name passed to getSymbol');
+
+	const group = usergroups[userid] || ' ';
+	return group + name;
+}
+
+export const Conditions: {[k: string]: ModdedConditionData & {innateName?: string}} = {
+	/*
+	// Example:
+	userid: {
+		noCopy: true,
+		onStart() {
+			this.add(`c|${getName('Username')}|Switch In Message`);
+		},
+		onSwitchOut() {
+			this.add(`c|${getName('Username')}|Switch Out Message`);
+		},
+		onFaint() {
+			this.add(`c|${getName('Username')}|Faint Message`);
+		},
+		// Innate effects go here
+	},
+	IMPORTANT: Obtain the username from getName
+	*/
+	// Please keep statuses organized alphabetically based on staff member name!
+	
+};
---
+++ data/mods/stb/random-teams.ts
@@ -0,0 +1,119 @@
+import RandomTeams from '../../random-teams';
+
+export interface SSBSet {
+	species: string;
+	ability: string | string[];
+	item: string | string[];
+	gender: GenderName;
+	moves: (string | string[])[];
+	signatureMove: string;
+	evs?: {hp?: number, atk?: number, def?: number, spa?: number, spd?: number, spe?: number};
+	ivs?: {hp?: number, atk?: number, def?: number, spa?: number, spd?: number, spe?: number};
+	nature?: string | string[];
+	shiny?: number | boolean;
+	level?: number;
+	happiness?: number;
+	skip?: string;
+}
+interface SSBSets {[k: string]: SSBSet}
+
+export const ssbSets: SSBSets = {
+	/*
+	// Example:
+	Username: {
+		species: 'Species', ability: 'Ability', item: 'Item', gender: '',
+		moves: ['Move Name', ['Move Name', 'Move Name']],
+		signatureMove: 'Move Name',
+		evs: {stat: number}, ivs: {stat: number}, nature: 'Nature', level: 100, shiny: false,
+	},
+	// Species, ability, and item need to be captialized properly ex: Ludicolo, Swift Swim, Life Orb
+	// Gender can be M, F, N, or left as an empty string
+	// each slot in moves needs to be a string (the move name, captialized properly ex: Hydro Pump), or an array of strings (also move names)
+	// signatureMove also needs to be capitalized properly ex: Scripting
+	// You can skip Evs (defaults to 82 all) and/or Ivs (defaults to 31 all), or just skip part of the Evs (skipped evs are 0) and/or Ivs (skipped Ivs are 31)
+	// You can also skip shiny, defaults to false. Level can be skipped (defaults to 100).
+	// Nature needs to be a valid nature with the first letter capitalized ex: Modest
+	*/
+	// Please keep sets organized alphabetically based on staff member name!
+	
+};
+
+export class RandomStaffBrosTeams extends RandomTeams {
+	randomStaffBrosTeam(options: {inBattle?: boolean} = {}) {
+		const team: PokemonSet[] = [];
+		const debug: string[] = []; // Set this to a list of SSB sets to override the normal pool for debugging.
+		const pool = debug.length ? debug : Object.keys(ssbSets);
+		const typePool: {[k: string]: number} = {};
+		let depth = 0;
+		while (pool.length && team.length < 6) {
+			if (depth >= 200) throw new Error(`Infinite loop in Super Staff Bros team generation.`);
+			depth++;
+			const name = this.sampleNoReplace(pool);
+			const ssbSet: SSBSet = this.dex.deepClone(ssbSets[name]);
+			if (ssbSet.skip) continue;
+
+			// Enforce typing limits
+			if (!debug.length) { // Type limits are ignored when debugging
+				const types = this.dex.getSpecies(ssbSet.species).types;
+				let rejected = false;
+				for (const type of types) {
+					if (typePool[type] === undefined) typePool[type] = 0;
+					if (typePool[type] >= 2) {
+						// Reject
+						rejected = true;
+						break;
+					}
+				}
+				if (ssbSet.ability === 'Wonder Guard') {
+					if (!typePool['wonderguard']) {
+						typePool['wonderguard'] = 1;
+					} else {
+						rejected = true;
+					}
+				}
+				if (rejected) continue;
+				// Update type counts
+				for (const type of types) {
+					typePool[type]++;
+				}
+			}
+
+			const set: PokemonSet = {
+				name: name,
+				species: ssbSet.species,
+				item: Array.isArray(ssbSet.item) ? this.sampleNoReplace(ssbSet.item) : ssbSet.item,
+				ability: Array.isArray(ssbSet.ability) ? this.sampleNoReplace(ssbSet.ability) : ssbSet.ability,
+				moves: [],
+				nature: ssbSet.nature ? Array.isArray(ssbSet.nature) ? this.sampleNoReplace(ssbSet.nature) : ssbSet.nature : 'Serious',
+				gender: ssbSet.gender || this.sample(['M', 'F', 'N']),
+				evs: ssbSet.evs ? {...{hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0}, ...ssbSet.evs} :
+				{hp: 84, atk: 84, def: 84, spa: 84, spd: 84, spe: 84},
+				ivs: {...{hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31}, ...ssbSet.ivs},
+				level: ssbSet.level || 100,
+				happiness: typeof ssbSet.happiness === 'number' ? ssbSet.happiness : 255,
+				shiny: typeof ssbSet.shiny === 'number' ? this.randomChance(1, ssbSet.shiny) : !!ssbSet.shiny,
+			};
+			while (set.moves.length < 3 && ssbSet.moves.length > 0) {
+				let move = this.sampleNoReplace(ssbSet.moves);
+				if (Array.isArray(move)) move = this.sampleNoReplace(move);
+				set.moves.push(move);
+			}
+			set.moves.push(ssbSet.signatureMove);
+
+			// Any set specific tweaks occur here.
+			if (set.name === 'Marshmallon' && !set.moves.includes('Head Charge')) set.moves[this.random(3)] = 'Head Charge';
+
+			team.push(set);
+
+			// Team specific tweaks occur here
+			// Swap last and second to last sets if last set has Illusion
+			if (team.length === 6 && set.ability === 'Illusion') {
+				team[5] = team[4];
+				team[4] = set;
+			}
+		}
+		return team;
+	}
+}
+
+export default RandomStaffBrosTeams;
