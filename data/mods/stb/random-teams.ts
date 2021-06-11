6eb4525053ba6f02c2a5dc4ab00752af72dc5114
Br(i)an Flakes <brh3243@rit.edu>
2021-06-10 21:31:17 -0400

    removed depug stream

--- data/mods/stb/random-teams.ts
+++ data/mods/stb/random-teams.ts
@@ -245,7 +245,7 @@ export const stbSets: STBSets = {
 export class RandomStaffBrosTeams extends RandomTeams {
 	randomStaffBrosTeam(options: {inBattle?: boolean} = {}) {
 		const team: PokemonSet[] = [];
-		const debug: string[] = ["Banded Bonks", "TacocaT_2595"]; // Set this to a list of SSB sets to override the normal pool for debugging.
+		const debug: string[] = []; // Set this to a list of SSB sets to override the normal pool for debugging.
 		const pool = debug.length ? debug : Object.keys(stbSets);
 		const typePool: {[k: string]: number} = {};
 		let depth = 0;
